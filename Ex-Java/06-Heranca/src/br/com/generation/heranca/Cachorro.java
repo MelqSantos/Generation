b1582c17cfbc724405c80a88badfa167b033bad1
Melqui Santos <melqsantos96@gmail.com>
2021-11-16 18:41:54 -0300

    Heranças em POO

---
+++ Ex-Java/06-Heranca/src/br/com/generation/heranca/Cachorro.java
@@ -0,0 +1,8 @@
+package br.com.generation.heranca;
+
+public class Cachorro extends Animal{
+	
+	void correr() {
+		System.out.println("Este tipo de animal pode correr!");
+	}
+}
---
+++ Ex-Java/06-Heranca/src/br/com/generation/heranca/Cavalo.java
@@ -0,0 +1,8 @@
+package br.com.generation.heranca;
+
+public class Cavalo extends Animal{
+	
+	void correr() {
+		System.out.println("Este tipo de animal pode correr!");
+	}
+}
